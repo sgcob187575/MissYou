a910bfdcf7d78917e5369e205b3d1d8f481fb932
YuHao <sgcob187575@gmail.com>
2020-03-14 16:25:04 +0800

    Initial Commit

---
+++ MissYou/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MissYou
+//
+//  Created by 陳昱豪 on 2020/3/14.
+//  Copyright © 2020 Chen_Yu_Hao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
